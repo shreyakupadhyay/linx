fd27f3832233f157e544f271f194e8a784669e1f
Gaurav Koley <arkokoley@live.in>
2016-10-12 15:33:55 +0530

    The Beginning

---
+++ app.js
@@ -0,0 +1,11 @@
+// BASE SETUP
+// =============================================================================
+
+var express = require('express'),
+	bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser());
+
+var env = app.get('env') == 'development' ? 'dev' : app.get('env');
+var port = process.env.PORT || 8080;
